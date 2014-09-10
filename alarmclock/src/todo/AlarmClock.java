2702fc3602fda9cc7dda7b4d9a50990f217a78fc
Oskar holmberg <oskarholmberg@msn.com>
2014-09-10 13:09:46 +0200

    Alarm Clock

--- alarmclock/src/todo/AlarmClock.java
+++ alarmclock/src/todo/AlarmClock.java
@@ -7,7 +7,10 @@ public class AlarmClock extends Thread {
 
 	private static ClockInput	input;
 	private static ClockOutput	output;
-	private static Semaphore	sem; 
+	private static Semaphore	sem;
+    private int alarmTime;
+    private boolean alarmOn = false;
+
 
 	public AlarmClock(ClockInput i, ClockOutput o) {
 		input = i;
@@ -18,12 +21,21 @@ public class AlarmClock extends Thread {
 	// The AlarmClock thread is started by the simulator. No
 	// need to start it by yourself, if you do you will get
 	// an IllegalThreadStateException. The implementation
-	// below is a simple alarmclock thread that beeps upon 
+	// below is a simple alarmclock thread that beeps upon
 	// each keypress. To be modified in the lab.
 	public void run() {
-		while (true) {
-			sem.take();
-			output.doAlarm();
-		}
-	}
+        Ticker tick = new Ticker(input, output);
+        tick.start();
+
+        while (true) {
+            sem.give();
+            if(input.getChoice()==1){
+                alarmTime = input.getValue();
+            }
+            if(input.getAlarmFlag()&&alarmTime==tick.getTime()){
+                alarmOn=true;
+            }
+            sem.take();
+        }
+    }
 }
